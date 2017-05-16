aff206aecf618166deb5b3b0447d0f68876721cd
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-05-16 14:33:23 -0400

    got checkbox incrament function working

--- interface.js
+++ interface.js
@@ -6,40 +6,46 @@
   function checkLength (eventObj) {
     console.log(eventObj.target.value);
     if (eventObj.target.value.length >0) {
-        eventObj.target.parentNode.parentNode.classList.remove('has-error');
+      eventObj.target.parentNode.parentNode.classList.remove('has-error');
     } else {
       eventObj.target.parentNode.parentNode.classList.add('has-error');
     }
   }
 
   document.querySelector('#full-name')
-    .addEventListener( 'blur', checkLength );
+  .addEventListener( 'blur', checkLength );
 
 
   function figureUpdate (eventObj) {
-    eventObj.target.parentNode.childNodes[3]
-    .innerText = eventObj.target.value + 'years';
-      // console.log(eventObj.target.parentNode.childNodes);
+     eventObj.target.parentNode.childNodes[3]
+    .innerText = eventObj.target.value + ' years';
   }
+
 
   document.querySelector('#exp')
   .addEventListener ( 'change', figureUpdate);
 
-  function checkboxCount (eventObj) {
-    let checkboxCount = 0;
-    
-    if (each.childNodes[1].checked) {
-  }
-    console.log(eventObj.target.checked);
 
-  }
+  let checkbox = 0;
 
 
-  Array.from( document.querySelectorAll('[name="languages"]'))
-    .forEach(function eachBox (box) {
-      box.addEventListener( 'change', checkboxCount );
+  Array.from(document.querySelectorAll("[name=languages]")).forEach(function checkStatus(each) {
+    each.addEventListener("change", function countLang(eventObj) {
+      if (eventObj.target.checked === true) {
+        checkbox++;
+        console.log(checkbox);
+      } else {
+        languageCount--;
+        console.log(checkbox);
+      }
+      eventObj.target.parentNode.parentNode.childNodes[7].innerText = checkbox + " languages";
     });
+  });
 
+  document.querySelector("form")
+    .addEventListener("submit", function stopSubmit(e) {
+      eventObj.preventDefault();
+    });
 
 
 }());
